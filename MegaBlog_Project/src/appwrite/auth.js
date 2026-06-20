bba5ef582e660f0c2a0aeef2111615c5da3d7790
parthnilmakwana <parthnilmakwana@gmail.com>
2026-06-20 17:45:42 +0530

    setup react-reduxtoolkit in store

--- MegaBlog_Project/src/appwrite/auth.js
+++ MegaBlog_Project/src/appwrite/auth.js
@@ -6,7 +6,7 @@ export class AuthService {
   account;
   constructor() {
     this.client
-      .setEndpoint(conf.appwriterUrl)
+      .setEndpoint(conf.appwriteUrl)
       .setProject(conf.appwriteProjectId);
 
     this.account = new Account(this.client);
