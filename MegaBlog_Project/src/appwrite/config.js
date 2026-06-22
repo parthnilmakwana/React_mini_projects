8d648a52c5b12600c9de1a7958f46cc25e093941
parthnilmakwana <parthnilmakwana@gmail.com>
2026-06-23 00:51:56 +0530

    fix: correct appwriteUrl in Service class and implement routing in main.jsx with AuthLayout for authentication

--- MegaBlog_Project/src/appwrite/config.js
+++ MegaBlog_Project/src/appwrite/config.js
@@ -7,7 +7,7 @@ export class Service {
   bucket;
   constructor() {
     this.client
-      .setEndpoint(conf.appwriterUrl)
+      .setEndpoint(conf.appwriteUrl)
       .setProject(conf.appwriteProjectId);
 
     this.databases = new Databases(this.client);
